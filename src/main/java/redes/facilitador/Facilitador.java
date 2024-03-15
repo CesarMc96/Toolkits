c88b176ab970c2aa7f26b60843a3730b66f3bf33
César Alejandro Montaño Cortés <cesarmc96@hotmail.com>
2024-03-15 17:19:43 -0600

    Version Pantalla

--- src/main/java/redes/facilitador/Facilitador.java
+++ src/main/java/redes/facilitador/Facilitador.java
@@ -11,6 +11,6 @@ package redes.facilitador;
 public class Facilitador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Automatizar().setVisible(true);
     }
 }
